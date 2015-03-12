a6fd20d604689147b4ade9dcf65d5bf8cbd7d52c
Everton Custodio <evertonhenrique8@gmail.com>
2015-03-11 22:56:35 -0300

    Utilizando o converter no VotacaoController.

--- src/main/java/br/eti/evertoncustodio/votenolivro/controller/VotacaoController.java
+++ src/main/java/br/eti/evertoncustodio/votenolivro/controller/VotacaoController.java
@@ -2,27 +2,21 @@ package br.eti.evertoncustodio.votenolivro.controller;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.eti.evertoncustodio.votenolivro.model.Livro;
 import br.eti.evertoncustodio.votenolivro.model.Votacao;
-import br.eti.evertoncustodio.votenolivro.model.dao.LivroDAO;
 
 @Controller
 public class VotacaoController {
 	
-	@Autowired
-	private LivroDAO livroDAO;
-	
 	@Transactional
 	@RequestMapping("votar.do")
-	public String votar(Model model, HttpSession session, Long id) {
-		Livro livroSelecionado = livroDAO.getLivro(id);
-		
+	public String votar(Model model, HttpSession session, @RequestParam("id") Livro livroSelecionado) {
 		Votacao votacao = (Votacao) session.getAttribute("votacao");
 		votacao.atual().votarNo(livroSelecionado);
 		
@@ -34,9 +28,5 @@ public class VotacaoController {
 		model.addAttribute("opcao2", votacao.atual().getOpcoes().get(1));
 		
 		return "opcoes";
-	}
-
-	public void setLivroDAO(LivroDAO livroDAO) {
-		this.livroDAO = livroDAO;
 	}
 }
